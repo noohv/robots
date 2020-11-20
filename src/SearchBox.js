0f830b504aba8113c2e7ac68d857a127a948cd3e
noohv <56311522+noohv@users.noreply.github.com>
2020-11-20 15:08:32 +0200

    added colors

--- src/SearchBox.js
+++ src/SearchBox.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import './index.css';
 
 const SearchBox = ({ searchChange }) => {
     return (
         <div className='pa2'>
             <input
-            className='pa3 ba b--green bg-lightest-blue' 
+            className='pa3 search' 
             type='search' 
             placeholder='Search'
             onChange={searchChange}
